cd30cc4b7708d3584b1040535de5f3f8e35d39cc
r3xyzz <rexgamer152@gmail.com>
2023-05-31 15:08:57 -0400

    acitividad2

--- Act2Ejercicio1.py
+++ Act2Ejercicio1.py
@@ -1,8 +1,34 @@
 ''' 1.1 Crear un arreglo //unidimensional// de tamaño 10, conelementos aleatorios de números enteros del 0 al 100, 
 para ello deberá investigar la función que permita crear números aleatorios.
 '''
-import random #generador de numeros aleatorios para la varible
 
-numero_aleatorio = random.randint(1, 10)
-print("el numero enetero aleatorio es: ",numero_aleatorio)
+''' 1.2. Crear una copia del arreglo y muestre:
+o Elemento mayoro 
+o Elemento menoro 
+o Suma de los elementoso 
+o Promedio de los elementoso 
+o Mostrar todos los elementos 
+'''
+
+import numpy as np
+import random as rd
+
+# arreglo = [] esto es una lista (de cualquier tipo)
+print("---------------------------------------------------")
+arreglo = np.zeros(10) # es un arreglo 
+# print(arreglo)
+
+for i in range(len(arreglo)):
+    arreglo[i] = rd.randint(1,100)
+print(arreglo)
+
+arregloDos = arreglo[:].copy()
+print("---------------------------------------------------")
+
+print("El numero mayor del arregloDos : ", arregloDos.max())
+print("El numero menor del arregloDos : ", arregloDos.min())
+print("La suma de los elementos del arregloDos : ", arregloDos.sum())
+print("El promedio de sus elementos del arregloDos : ", arregloDos.mean())
+
+print("fin")
 
