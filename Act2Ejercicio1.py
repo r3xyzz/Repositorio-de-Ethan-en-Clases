22c6f4a73fdd0194d52757653aee04717bb170e8
r3xyzz <rexgamer152@gmail.com>
2023-05-31 14:49:13 -0400

    ejercicio1act2

---
+++ Act2Ejercicio1.py
@@ -0,0 +1,8 @@
+''' 1.1 Crear un arreglo //unidimensional// de tamaño 10, conelementos aleatorios de números enteros del 0 al 100, 
+para ello deberá investigar la función que permita crear números aleatorios.
+'''
+import random #generador de numeros aleatorios para la varible
+
+numero_aleatorio = random.randint(1, 10)
+print("el numero enetero aleatorio es: ",numero_aleatorio)
+
