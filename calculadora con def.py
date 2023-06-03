557e075e5b73a86e46b561ce7e070be42c0a70de
Ethan <127980069+r3xyzz@users.noreply.github.com>
2023-06-03 00:12:00 -0400

    calculadora con def esta vez

---
+++ calculadora con def.py
@@ -0,0 +1,31 @@
+def suma(num1, num2):
+    return num1 + num2 
+
+def resta(num1, num2):
+    return num1 - num2 
+
+def multiplicacion(num1, num2):
+    return num1 * num2
+
+def divicion(num1, num2):
+    if num2 != 0:
+        return num1 / num2
+    else:
+        return "Error compañero, no se puede dividir entre cero."
+
+print("--------------------------------------------")
+num1 = float(input("Ingrese el primer numero: "))
+num2 = float(input("Ingrese el segundo numero: "))
+print("--------------------------------------------")
+
+resultado_suma = suma(num1, num2)
+resultado_resta = resta(num1, num2)
+resultado_multiplicacion = multiplicacion(num1, num2)
+resultado_divicion = divicion(num1, num2)
+
+print("////////////////////////")
+print("Suma: ",resultado_suma)
+print("Resta: ",resultado_resta)
+print("Multiplicacion: ",resultado_multiplicacion)
+print("Divicion: ",resultado_divicion)
+print("////////////////////////")
