48db6217b3ee2c63ed832fe7a13b22c667de4262
Ethan <127980069+r3xyzz@users.noreply.github.com>
2023-06-10 14:35:42 -0400

    contador / vocales

--- EjercicioRepaso2.py
+++ EjercicioRepaso2.py
@@ -2,3 +2,12 @@
 Contador de vocales: Escribe un programa que cuente la cantidad de vocales en una palabra ingresada por el usuario.
 '''
 
+palabra = input("Ingrese una palabra: ")
+
+contador = 0 
+
+for letra in palabra:
+    if letra.lower() in "aeiou":
+        contador += 1
+
+print("La cantidad de vocales en la palabra es: ", contador)
