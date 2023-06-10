502362b617f7c157a5552e8594c715e4c540fcc2
Ethan <127980069+r3xyzz@users.noreply.github.com>
2023-06-10 14:24:44 -0400

    EjercicioRepasito1

---
+++ EjercicioRepaso1.py
@@ -0,0 +1,16 @@
+'''
+Calculadora básica: Crea un programa que solicite al usuario dos números y realice las operaciones básicas 
+(suma, resta, multiplicación y división) con ellos.
+'''
+numero1 = float(input("Ingrese el primer numero: "))
+numero2 = float(input("ingrese el segundo numero: "))
+
+suma = numero1 + numero2
+resta = numero1 - numero2
+multiplicacion = numero1 * numero2
+divicion = numero1 / numero2
+
+print("La suma es =", suma)
+print("La resta es = ", resta)
+print("La multiplicacion es = ",multiplicacion)
+print("La divicion es = ", divicion)
