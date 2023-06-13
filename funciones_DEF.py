702fbec7eaf861ae2e26f462b295970f3909ff42
Ethan <127980069+r3xyzz@users.noreply.github.com>
2023-06-13 16:59:34 -0400

    otro arrgelo estetico de las respuestas

--- funciones_DEF.py
+++ funciones_DEF.py
@@ -30,21 +30,35 @@ def calcularIMC(peso,estatura):
     imc = peso/estatura**2
 
     if imc < 18.5:
+        print("/////////")
         print("Bajo Peso")
+        print("/////////")
     else:
         if imc >= 18.5 and imc <= 24.9:
+            print("/////////")
             print("Adecuado")
+            print("/////////")
         else:
             if imc >= 25.9 and imc <= 29.9:
+                print("/////////")
                 print("Sobrepeso")
+                print("/////////")
             else:
                 if imc >=30 and imc <= 34.9:
+                    print("////////////////")
                     print("Obesidad grado 1")
+                    print("////////////////")
                 else:
                     if imc >= 35 and imc <= 39.9:
+                        print("////////////////")
                         print("Obesidad grado 2")
+                        print("////////////////")
                     else:
                         if imc > 40:
+                            print("////////////////")
                             print("Obesidad grado 3")
+                            print("////////////////")
                         else:
+                            print("--------------")
                             print("Sin registros!")
+                            print("--------------")
