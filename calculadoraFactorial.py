62cf5c955f26ee325295e02153b04c5bce9d6865
Ethan <127980069+r3xyzz@users.noreply.github.com>
2023-07-02 20:46:37 -0400

    calculadora factorial

---
+++ calculadoraFactorial.py
@@ -0,0 +1,9 @@
+def calcular_factorial(numero):
+    factorial = 1 
+    for i in range(1, numero + 1):
+        factorial *= 1
+    return factorial
+
+numero_ingresado = int(input("Ingresar algun numero: "))
+factorial_resultado = calcular_factorial(numero_ingresado)
+print(f"El factorial de {numero_ingresado} es: {factorial_resultado}")
